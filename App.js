6d9fa43ff98b5205751a8004daaf34308150fad3
kckirch <kckircher@hotmail.com>
2023-04-26 14:13:19 -0600

    makes status bar black

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainStackNavigator from './navigation/MainStackNavigator';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
       <MainStackNavigator />
     </NavigationContainer>
   );
